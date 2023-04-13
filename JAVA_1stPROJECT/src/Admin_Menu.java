59618bee4160de4ffa96f936a8f476ab126c85b3
devjangdae <tjqkd0912@gmail.com>
2023-04-13 12:07:32 +0900

    Modify: Admin_Menu.java checkAttendance()

--- JAVA_1stPROJECT/src/Admin_Menu.java
+++ JAVA_1stPROJECT/src/Admin_Menu.java
@@ -14,6 +14,7 @@ public class Admin_Menu extends Menu {
 
 	@Override
 	void checkAttendance() {
+		
 		// 관리자는 모든 학생의 출결정보를 조회할 수 있다
 		// 1. 관리자는 학생 출결 조회 메뉴로 간다.
 		// 2. 관리자는 학생들의 출결을 조회한다.
@@ -22,28 +23,28 @@ public class Admin_Menu extends Menu {
 		Scanner sc = new Scanner(System.in);
 		boolean run = false;
 
-		// 출석파일 정보 얻기 //임시 의사코드
-		File file = new File("출석파일.txt");
-		
-		try {
-			FileInputStream fis = new FileInputStream(file);
-			ObjectInputStream oos = new ObjectInputStream(fis);
-
-			account_dic = (HashMap) oos.readObject(); 
-
-			Set<String> set = account_dic.keySet();
-			for (String v : set) { 
-				String name = account_dic.get(v).getName();
-				String attendance = account_dic.get(v).getAttendance();
-			}
-
-			oos.close();
-			fis.close();
-
-		} catch (Exception e) {
-			System.out.println("출석 정보 파일을 불러오는데 실패하였습니다.");
-			e.printStackTrace();
-		}
+		// 출석파일 정보 얻기 
+//		File file = new File("출석파일.txt");
+//		
+//		try {
+//			FileInputStream fis = new FileInputStream(file);
+//			ObjectInputStream oos = new ObjectInputStream(fis);
+//
+//			account_dic = (HashMap) oos.readObject(); 
+//
+//			Set<String> set = account_dic.keySet();
+//			for (String v : set) { 
+//				String name = account_dic.get(v).getName();
+//				String attendance = account_dic.get(v).getAttendance();
+//			}
+//
+//			oos.close();
+//			fis.close();
+//
+//		} catch (Exception e) {
+//			System.out.println("출석 정보 파일을 불러오는데 실패하였습니다.");
+//			e.printStackTrace();
+//		}
 
 		// 출결조회 기능
 		while (!run) {
--- JAVA_1stPROJECT/src/Student_Menu.java
+++ JAVA_1stPROJECT/src/Student_Menu.java
@@ -177,35 +177,6 @@ public class Student_Menu extends Menu {
 
 	}
 
-	public void login() {
-
-		boolean found = false;
-		System.out.println("로그인을 합니다.");
-
-		System.out.println("아이디를 입력하세요: ");
-		String accountId = sc.nextLine();
-
-		System.out.println("비밀번호를 입력하세요: ");
-		String passWord = sc.nextLine();
-		
-		for(int i = 0; i < accounts.size(); i++) {
-			if( accountId == accounts.get(i).getAccountId()) {
-				if(passWord == accounts.get(i).getPassWord()) {
-					System.out.println("로그인 완료");
-					found = true;
-					//showMenu
-				} else {
-					System.out.println("비밀번호가 틀립니다.");
-				}
-			}
-		}
-		
-		if(!found) {
-			System.out.println("입력한 아이디가 존재하지 않습니다.");
-		}
-		
-	}
-
 	// 이메일주소 일치 확인
 //	public boolean findEmail(String email) {
 //		for (Account ac : this.accounts) {
