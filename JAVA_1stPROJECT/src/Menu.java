b7b71f44652c52d7a45b6657d2d92f4a860bcf6f
Gunhee Lee <rjsgml9799@gmail.com>
2023-04-14 13:32:13 +0900

    Add:Method on Admin_Menu

--- JAVA_1stPROJECT/src/Menu.java
+++ JAVA_1stPROJECT/src/Menu.java
@@ -93,7 +93,8 @@ public abstract class Menu {
 			map = (HashMap) oos.readObject();
 			//System.out.println("불러온 유저수 : " + map.size());
 		} catch (Exception e) {
-			e.printStackTrace();
+//			e.printStackTrace();
+			System.out.println("첫번째 회원이 되어보세요");
 		}
 	}
 	
